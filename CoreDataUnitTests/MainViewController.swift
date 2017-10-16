95c28827bd4bb068d1d8d31177c1fb396c3d1e4b
Luke Smith <lukecsmith@gmail.com>
2017-10-16 10:20:48 +0100

    Initial project with ViewController and ViewModel.

---
+++ CoreDataUnitTests/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  CoreDataUnitTests
+//
+//  Created by Luke Smith on 14/10/2017.
+//  Copyright © 2017 LukeSmith. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    let viewModel = MainViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoreDataUnitTests/MainViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewModel.swift
+//  CoreDataUnitTests
+//
+//  Created by Luke Smith on 16/10/2017.
+//  Copyright © 2017 LukeSmith. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class MainViewModel {
+    
+    //directly injected from AppDelegate on startup.
+    var context : NSManagedObjectContext? = nil
+    
+}
